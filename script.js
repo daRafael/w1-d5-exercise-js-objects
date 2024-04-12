01053697eab91e3da16c71c1864bfe01dbee1043
Rafael Guerra <rafa99gm@hotmail.com>
2024-04-12 10:47:59 +0100

    solved

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 console.log("Ready to go! 🚀");
+
+const userInfo = {
+  userId : 1,
+  fullName: 'Rafael Guerra',
+}
+
+const book1 = {
+  title: '1984',
+  author: 'George Orwell',
+  isbn: '',
+  category: 'Dystopian'
+
+}
+
+const book2 = {
+  title: 'Mice and men',
+  author: 'John Steinbeck',
+  isbn: '',
+  category: 'drama'
+}
+
+
+userInfo.books = [book1, book2];
+
+const library = [];
+
+library.push(userInfo);
+
+
+
+const book3 = {
+  title: 'Mice and men',
+  author: 'John Steinbeck',
+  isbn: '',
+  category: 'drama'
+}
+
+library[0].books.push(book3);
+
+library.forEach(user => {
+  user.books.forEach(book => {
+    console.log(book.title);
+  })
+
+})
